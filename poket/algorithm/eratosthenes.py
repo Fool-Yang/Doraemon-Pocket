c4b487c768a6156097e2247ee8164c23fb8973af
Yang <39217646+Fool-Yang@users.noreply.github.com>
2019-06-11 15:47:22 -0700

    fixed Eratosthenes and FME

--- poket/algorithm/eratosthenes.py
+++ poket/algorithm/eratosthenes.py
@@ -1,8 +1,7 @@
 def Eratosthenes(n):
     IsPrime = [True] * (n + 1)
-    IsPrime[1] = False
     for i in range(2, int(n ** 0.5) + 1):
         if IsPrime[i]:
             for j in range(i ** 2, n + 1, i):
                 IsPrime[j] = False
-    return {i for i in range(2, n + 1) if IsPrime[i]}
+    return [i for i in range(2, n + 1) if IsPrime[i]]
