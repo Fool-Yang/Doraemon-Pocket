7eb92bb24e8729c6e8d06be62971155fd8338d14
Yang <leo19970804@gmail.com>
2025-03-29 22:55:29 +0000

    Update max_flow.py

--- poket/algorithm/graph_theory/max_flow.py
+++ poket/algorithm/graph_theory/max_flow.py
@@ -1,29 +1,29 @@
 from collections import deque
 
-def MaxFlow(G):
+def MaxFlow(G, s, t):
     # construct residual graph with flow/cap
     Gr = [{} for _ in range(len(G))]
     for u in range(len(G)):
         for v in G[u]:
             Gr[u][v] = [0, G[u][v]]
-            Gr[v][u] = [0, 0]
+            if u not in Gr[v]:
+                Gr[v][u] = [0, 0]
     # find augmenting path until fails
     total_flow = 0
-    sink = len(Gr) - 1
     augmentingPathFound = True
     while augmentingPathFound:
         augmentingPathFound = False
         # BFS
         Prev = [None]*len(Gr)
-        Prev[0] = len(Gr)
-        Queue = deque([0])
+        Prev[s] = len(Gr)
+        Queue = deque([s])
         while Queue:
             u = Queue.popleft()
             for v in Gr[u]:
                 flow, cap = Gr[u][v]
                 if Prev[v] is None and flow < cap:
                     Prev[v] = u
-                    if v == sink:
+                    if v == t:
                         augmentingPathFound = True
                         break
                     Queue.append(v)
@@ -35,16 +35,16 @@ def MaxFlow(G):
         if augmentingPathFound:
             # find bottleneck
             inc = float("inf")
-            curr = sink
-            while curr != 0:
+            curr = t
+            while curr != s:
                 flow, cap = Gr[Prev[curr]][curr]
                 room = cap - flow
                 if room < inc:
                     inc = room
                 curr = Prev[curr]
             # push more flow
-            curr = sink
-            while curr != 0:
+            curr = t
+            while curr != s:
                 Gr[Prev[curr]][curr][0] += inc
                 Gr[curr][Prev[curr]][0] -= inc
                 curr = Prev[curr]
